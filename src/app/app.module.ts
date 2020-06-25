25f10b6218d10712b1c1b118119364b085033dd1
Daan Vankerkom <daan@vankerkom.be>
2020-06-25 09:32:51 +0200

    Add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const COMPONENTS = [];
+
+@NgModule({
+  declarations: [COMPONENTS],
+  exports: [COMPONENTS],
+  imports: [CommonModule],
+})
+export class MaterialModule {}
