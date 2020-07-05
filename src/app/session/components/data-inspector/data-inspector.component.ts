e6758afd07c76047734fe401b7dc001cbc2e102c
Daan Vankerkom <daan@vankerkom.be>
2020-07-05 23:01:38 +0200

    Fix data inspector

--- src/app/session/components/data-inspector/data-inspector.component.ts
+++ src/app/session/components/data-inspector/data-inspector.component.ts
@@ -25,7 +25,7 @@ export class DataInspectorComponent implements OnInit {
   length: number = 0;
   binary: string | null = null;
 
-  @Input() littleEndian = true;
+  @Input() littleEndian: boolean;
 
   @Input() set buffer(buffer: any) {
     const dataView = new DataView(buffer || new ArrayBuffer(0));
@@ -33,17 +33,17 @@ export class DataInspectorComponent implements OnInit {
     this.length = dataView.byteLength;
 
     this.uint8Value = dataView.byteLength >= 1 ? dataView.getUint8(0) : null;
-    this.uint16Value = dataView.byteLength >= 2 ? dataView.getUint16(0, this.littleEndian) : null;
-    this.uint32Value = dataView.byteLength >= 4 ? dataView.getUint32(0, this.littleEndian) : null;
+    this.uint16Value = dataView.byteLength >= 2 ? dataView.getUint16(0, !!this.littleEndian) : null;
+    this.uint32Value = dataView.byteLength >= 4 ? dataView.getUint32(0, !!this.littleEndian) : null;
     // this.uint64Value = dataView.byteLength >= 8 ? dataView.getBigUint64(0, this.littleEndian) : null;
 
     this.int8Value = dataView.byteLength >= 1 ? dataView.getInt8(0) : null;
-    this.int16Value = dataView.byteLength >= 2 ? dataView.getInt16(0, this.littleEndian) : null;
-    this.int32Value = dataView.byteLength >= 4 ? dataView.getInt32(0, this.littleEndian) : null;
+    this.int16Value = dataView.byteLength >= 2 ? dataView.getInt16(0, !!this.littleEndian) : null;
+    this.int32Value = dataView.byteLength >= 4 ? dataView.getInt32(0, !!this.littleEndian) : null;
     // this.int64Value = dataView.byteLength >= 8 ? dataView.getBigInt64(0, this.littleEndian) : null;
 
-    this.float32Value = dataView.byteLength >= 4 ? dataView.getFloat32(0, this.littleEndian) : null;
-    this.float64Value = dataView.byteLength >= 8 ? dataView.getFloat64(0, this.littleEndian) : null;
+    this.float32Value = dataView.byteLength >= 4 ? dataView.getFloat32(0, !!this.littleEndian) : null;
+    this.float64Value = dataView.byteLength >= 8 ? dataView.getFloat64(0, !!this.littleEndian) : null;
 
     this.binary = this.uint8Value?.toString(2).padStart(8, '0') || null;
   }
