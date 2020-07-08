8e71704bbc411d8d4b6a2516473312d184591627
Daan Vankerkom <daan@vankerkom.be>
2020-07-08 17:44:27 +0200

    Redesign action bar

--- src/app/session/components/action-bar/action-bar.component.ts
+++ src/app/session/components/action-bar/action-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-action-bar',
   templateUrl: './action-bar.component.html',
-  styleUrls: ['./action-bar.component.scss']
+  styleUrls: ['./action-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActionBarComponent {
-
   @Input() active = false;
 
   @Output() start = new EventEmitter<void>();
   @Output() stop = new EventEmitter<void>();
-
 }
